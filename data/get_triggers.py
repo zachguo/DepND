f970c94dbca5d00400b4a46ed3527a71b682e8c1
Zach Guo <zachguo@gmail.com>
2013-06-12 14:31:14 -0400

    update data
    triggers & test cases

--- data/get_triggers.py
+++
@@ -1,7 +0,0 @@
-## find negation triggers
-
-with open("/Users/siyuanguo/GoogleDrive/Megaputer/NegDetection/DepNegEx/data/negex_triggers_raw.txt",'r') as fin:
-    with open("/Users/siyuanguo/GoogleDrive/Megaputer/NegDetection/DepNegEx/data/negex_triggers_trimmed.txt", "w") as fout:
-        for line in fin:
-            if "[PREN]" in line:
-                fout.write(line)
