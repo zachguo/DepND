b37ca12be4e2b88c054e2447204ac51ecf57fd08
Zach Guo <zachguo@gmail.com>
2013-06-18 12:19:35 -0400

    minor cleanup

--- NegDep.py
+++ NegDep.py
@@ -1,5 +1,6 @@
 ## NegDep
 import os
+import re
 
 class NegDep():
 
@@ -31,9 +32,11 @@ class NegDep():
                     line = line.strip()
                     if line:
                         for trigger in self.NegTriggers:
-                            if trigger in line:
+                            pattern = re.compile(r'\b'+trigger+r'\b')
+                            if pattern.search(line):
                                 tfout.write(line+"\n\n")
-                            break
+                                break
+                        ## something wrong here, write some sents containing no negation cues
 
     def parse(self):
         # run stanford parser to produce denpency trees
@@ -41,7 +44,7 @@ class NegDep():
         print "start parsing ..."
         os.system(cmd)
 
-    def visualize_ParsedTree(self):
+    def visualize_ParsedTree(self, i_start, i_end):
         # visualize the parsed output using DependenSee, one sent by one sent
         # save the visualization into .png
         # !!Use carefully!! DependenSee has minor bugs (cannot read some parsed tree correctly)
@@ -55,20 +58,22 @@ class NegDep():
                     fout_tmp.write(line+"\n")
                 else:
                     fout_tmp.close()
-                    # call DependenSee to visualize the parsed tree
-                    cmd = "java -cp ./stanford-parser-2013-04-05/DependenSee.jar:./stanford-parser-2013-04-05/stanford-parser.jar:./stanford-parser-2013-04-05/stanford-parser-2.0.5-models.jar com.chaoticity.dependensee.Main -t ./data/parsed_output.tmp ./data/parsed_output_visualization/"+str(i_sent)+".png"
-                    os.system(cmd)
+                    if i_sent >= i_start and i_sent <= i_end:
+                        # call DependenSee to visualize the parsed tree
+                        cmd = "java -cp ./stanford-parser-2013-04-05/DependenSee.jar:./stanford-parser-2013-04-05/stanford-parser.jar:./stanford-parser-2013-04-05/stanford-parser-2.0.5-models.jar com.chaoticity.dependensee.Main -t ./data/parsed_output.tmp ./data/parsed_output_visualization/"+str(i_sent)+".png"
+                        os.system(cmd)
                     fout_tmp = open("./data/parsed_output.tmp","w")
                     i_sent += 1
+        fout_tmp.close()
 
     def run(self):
         self.read_NegTriggers()
         self.read_TestFile()
         self.parse()
-        self.visualize_ParsedTree()
         
 ## test run
 trigger_filepath = "./data/negTriggers.txt"
 test_filepath = "/Users/siyuanguo/GoogleDrive/Corpora/NegationDetectionCorpora/cleaned/PlainTextNoTag/bioscope_abstracts_cleaned.txt"
 bioscope = NegDep(trigger_filepath, test_filepath)
 bioscope.run()
+bioscope.visualize_ParsedTree(50,100) #visualize parsed sentences with indice from 50 to 100
